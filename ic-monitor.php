ef5f2997400bfd8c4535457a602ab27707cf3311
Walker Leite <walker@incuca.com.br>
2018-08-10 11:17:49 -0300

    import scaffold

--- ic-monitor.php
+++ ic-monitor.php
@@ -1,4 +1,15 @@
 <?php
-/*
-Plugin Name: ic-monitor
-*/
+/**
+ * Plugin Name:     INCUCA Monitor
+ * Plugin URI:      https://incuca.net
+ * Description:     WordPress plugin to expose website data through REST API
+ * Author:          INUCA
+ * Author URI:      https://incuca.net
+ * Text Domain:     ic-monitor
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ic_Monitor
+ */
+
+// Your code starts here.
