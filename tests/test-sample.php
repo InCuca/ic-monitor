6cda967beb604573ccd28c85318b5b9c125828ae
Walker Leite <walker@incuca.com.br>
2018-08-10 12:02:33 -0300

    add doc

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,6 +2,9 @@
 /**
  * Class SampleTest
  *
+ * PHPUnit 5.1 doc https://phpunit.de/manual/5.1/en/index.html
+ * WP_UnitTestCase doc https://github.com/rnagle/wordpress-unit-tests/blob/master/includes/factory.php
+ *
  * @package Ic_Monitor
  */
 
