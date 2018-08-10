aec563e66f38e82dbc7ec1093c0202cac286d2b4
Walker Leite <walker@incuca.com.br>
2018-08-10 15:30:57 -0300

    fix phpcs errors

--- tests/ic-monitor.spec.php
+++ tests/ic-monitor.spec.php
@@ -17,12 +17,12 @@ class IcMonitor extends WP_UnitTestCase {
 	 * It returns number of blocked attacks through /incuca/v1/blocked-attacks
 	 */
 	function testReturnsBlockedAttacksThroughEndpoint() {
-		require(__DIR__ . '/../ic-monitor.php');
+		require( __DIR__ . '/../ic-monitor.php' );
 		$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-        $server = new $wp_rest_server_class;
-		$req = new WP_REST_Request( 'GET', '/wp-json/incuca/v1/blocked-attacks' );
-		$res = $server->dispatch($req);
-		$this->assertEquals(200, $res->status);
+		$server               = new $wp_rest_server_class;
+		$req                  = new WP_REST_Request( 'GET', '/wp-json/incuca/v1/blocked-attacks' );
+		$res                  = $server->dispatch( $req );
+		$this->assertEquals( 200, $res->status );
 
 	}
 }
